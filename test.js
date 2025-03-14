d3e3a383743a52e04c73e289c358faf7ae1e7398
edwardsqa <eddyayomide2000@gmail.com>
2025-03-14 01:58:43 +0100

    first commit

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 let day= "friday"
 
-// // console.log(day)
+console.log(day)
 var position= "5"
-// console.log(position)
+console.log(position)
 var output="today is friday.it is the fifth day of the week"
 console.log(output)
 console.log(day+position)
